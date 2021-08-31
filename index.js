c89cfe5a40cf0492da6e66351f504ca958bceaf0
tfkhdyt <ta142fa@gmail.com>
2021-08-31 18:26:25 +0700

    nambah inline keyboard menuju pahe

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const { Telegraf } = require('telegraf');
+const {
+  Telegraf,
+  Markup
+} = require('telegraf');
 const axios = require('axios');
-const { Keyboard, Key } = require('telegram-keyboard');
+const {
+  Keyboard,
+  Key
+} = require('telegram-keyboard');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -18,16 +24,49 @@ bot.on('text', (ctx) => {
     // console.log(res.data.Search);
     const hasilQuery = res.data.Search;
     const keyCallback = hasilQuery.map((film) => {
-     return Key.callback(`${film.Title} (${film.Year})`, film.imdbID);
+      return Key.callback(`${film.Title} (${film.Year})`, film.imdbID);
     });
-    const keyboard = Keyboard.make(keyCallback, { columns : 1 }).inline();
-    console.log(keyboard);
+    const keyboard = Keyboard.make(keyCallback, {
+      columns: 1
+    }).inline();
+    // console.log(keyboard);
     bot.telegram.sendMessage(ctx.chat.id, `Menampilkan film/series dengan judul "${movieQuery}":`, keyboard);
   });
 });
 
 bot.on('callback_query', (ctx) => {
-  console.log(ctx.callbackQuery.data)
-})
+  // console.log(ctx.callbackQuery.data);
+  const imdbID = ctx.callbackQuery.data;
+  axios.get(omdbAPI + '&i=' + imdbID)
+  .then(res => {
+    console.log(res.data);
+    const data = res.data;
+    const sendPhoto = ctx.replyWithPhoto({
+      url: data.Poster
+    }, {
+      caption: `*Judul* : ${data.Title}
+*Tahun* : ${data.Year}
+*Rated* : ${data.Rated}
+*Tgl. Rilis* : ${data.Released}
+*Durasi* : ${data.Runtime}
+*Genre* : ${data.Genre}
+*Sutradara* : ${data.Director}
+*Penulis* : ${data.Writer}
+*Aktor* : ${data.Actors}
+*Plot* :
+${data.Plot}
+*Bahasa* : ${data.Language}
+*Negara* : ${data.Country}
+*Awards* : ${data.Awards}
+*Produksi* : ${data.Production}
+*Metascore* : ${data.Metascore}
+*IMDB Rating* : ${data.imdbRating}`,
+      parse_mode: 'Markdown',
+      ...Markup.inlineKeyboard([ 
+        Markup.button.url('🔎 Cari di pahe.ph', `https://pahe.ph/?s=${imdbID}`)
+      ]) 
+    });
+  });
+});
 
 bot.launch();
