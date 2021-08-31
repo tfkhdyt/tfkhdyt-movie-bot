8e0b38ee3c1d4c84170aae2725f13887756dfe69
tfkhdyt <ta142fa@gmail.com>
2021-08-31 22:00:10 +0700

    nambah fitur search by year

--- index.js
+++ index.js
@@ -4,23 +4,55 @@ const axios = require('axios');
 const { Keyboard, Key } = require('telegram-keyboard');
 require('dotenv').config();
 
-//const bot = new Telegraf(process.env.BOT_TOKEN);
+// const bot = new Telegraf(process.env.BOT_TOKEN);
 const bot = new Composer();
 const omdbAPI = `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}`;
-let keyboard;
 
 bot.start((ctx) => ctx.reply(`Halo ${ctx.from.first_name}, selamat datang di @TFKHDYTMovieBot, ketikkan nama film/series yang ingin dicari untuk menampilkan detail dari film tersebut.`));
 
-bot.command('help', (ctx) => ctx.reply(`Kamu hanya perlu mengetik judul film/series saja`));
+bot.command('help', (ctx) => ctx.reply(`Pencarian Film:
+    Kamu hanya perlu mengetik judul film/series saja
+Contoh:
+    WandaVision
 
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+Pencarian Film Berdasarkan Tahun Rilis:
+    /year [tahun rilis] [judul film]
+Contoh:
+    /year 2021 What If...?`));
+
+bot.command('year', (ctx) => {
+  console.log(ctx.update.message.text.split(' '));
+  const args = ctx.update.message.text.split(' ');
+  const year = args[1];
+  let judulFilm = '';
+  for (let i = 2; i < args.length; i++) {
+    let separator = (i < args.length - 1) ? ' ' : '';
+    judulFilm += args[i] + separator;
+  }
+  console.log(`Tahun : ${year}\nJudul Film : ${judulFilm}`);
+  const movieQuery = encodeURI(judulFilm);
+  axios.get(omdbAPI + '&s=' + movieQuery + '&y=' + year)
+  .then(res => {
+    // console.log(res.data.Search);
+    if (res.data.Response == 'False') return ctx.reply('Hasil tidak ditemukan! Silakan masukkan judul film yang lebih spesifik.');
+    const hasilQuery = res.data.Search;
+    const keyCallback = hasilQuery.map((film) => {
+      return Key.callback(`${film.Title} (${film.Year})`, film.imdbID);
+    });
+    const keyboard = Keyboard.make(keyCallback, {
+      columns: 1
+    }).inline();
+    // console.log(keyboard);
+    ctx.reply(`Menampilkan film/series dengan judul "${judulFilm} (${year})":`, keyboard);
+  });
+});
 
 bot.on('text', (ctx) => {
   const movieQuery = encodeURI(ctx.message.text);
   axios.get(omdbAPI + '&s=' + movieQuery)
   .then(res => {
     // console.log(res.data.Search);
-    if (res.data.Response == 'False') return ctx.reply('Hasil tidak ditemukan! Silahkan masukkan judul film yang lebih spesifik.')
+    if (res.data.Response == 'False') return ctx.reply('Hasil tidak ditemukan! Silakan masukkan judul film yang lebih spesifik.');
     const hasilQuery = res.data.Search;
     const keyCallback = hasilQuery.map((film) => {
       return Key.callback(`${film.Title} (${film.Year})`, film.imdbID);
