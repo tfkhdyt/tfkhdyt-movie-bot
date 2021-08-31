db2e116a574fa4a084fed233ef4d6dd75b3d2bbb
tfkhdyt <ta142fa@gmail.com>
2021-08-31 14:28:30 +0700

    nambah dotenv

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const Telegraf = require('telegraf') // import telegram lib
-
-const bot = new Telegraf(process.env.BOT_TOKEN) // get the token from envirenment variable
-bot.start((ctx) => ctx.reply('Welcome')) // display Welcome text when we start bot
-bot.hears('hi', (ctx) => ctx.reply('Hey there')) // listen and handle when user type hi text
-bot.launch() // start
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
