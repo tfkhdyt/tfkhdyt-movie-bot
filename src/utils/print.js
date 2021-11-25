76e1d3f69f67ba0970cfc43810c105936442afc8
tfkhdyt <ta142fa@gmail.com>
2021-11-25 11:44:56 +0700

    hapus module yang tak terpakai

--- src/utils/print.js
+++ src/utils/print.js
@@ -1,8 +1,11 @@
 const moment = require('moment-timezone');
 
 module.exports = (str, username) => {
-  const timestamp = moment().locale('id').tz('Asia/Jakarta').format('hh:mm:ss ddd, DD MMM YY');   
+  const timestamp = moment()
+    .locale('id')
+    .tz('Asia/Jakarta')
+    .format('hh:mm:ss ddd, DD MMM YY');
   username = username != null ? username : '';
-  
+
   console.log(`[${timestamp}] ${username}${str}`);
 };
