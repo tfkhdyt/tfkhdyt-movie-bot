395118d381fdb63d6b5a86bdd7b118309874a0c0
tfkhdyt <ta142fa@gmail.com>
2021-11-25 11:22:11 +0700

    fix bug pada print.js (nullish assignment), lagi

--- src/utils/print.js
+++ src/utils/print.js
@@ -2,7 +2,7 @@ const moment = require('moment-timezone');
 
 module.exports = (str, username) => {
   const timestamp = moment().locale('id').tz('Asia/Jakarta').format('hh:mm:ss ddd, DD MMM YY');   
-  username ??= '';
+  username = username != null ? username : '';
   
   console.log(`[${timestamp}] ${username}${str}`);
 };
